61975d36bde41c0108692cecfbd70279705173e0
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2022-10-02 22:55:15 +0500

    1. Two Sum - https://leetcode.com/problems/two-sum/

---
+++ src/TwoSum/Solution.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\TwoSum;
+
+class Solution
+{
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        foreach ($nums as $index1 => $num1) {
+            foreach ($nums as $index2 => $num2) {
+                if ($index1 !== $index2 && ($num1 + $num2) === $target) {
+                    return [$index1, $index2];
+                }
+            }
+        }
+    }
+}
