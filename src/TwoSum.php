21440df273211befc0398ad9fbb3d50be4e1501b
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2022-10-04 01:10:01 +0500

    1. Two Sum - https://leetcode.com/problems/two-sum/ - Write php unit test - Refactor a code

--- src/TwoSum.php
+++ src/TwoSum.php
@@ -1,8 +1,19 @@
 <?php
 
-namespace App\TwoSum;
+namespace App;
 
-class Solution
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to
+ * target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ *
+ * Example 1:
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+class TwoSum
 {
     /**
      * @param array<array-key, int> $nums
--- tests/TwoSumTest.php
+++ tests/TwoSumTest.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace App\Tests;
+
+use App\TwoSum;
+use PHPUnit\Framework\TestCase;
+
+class TwoSumTest extends TestCase
+{
+    /** @dataProvider twoSumDataProvider */
+    public function testTwoSum(array $nums, int $target, array $expected): void
+    {
+        $twoSum = new TwoSum();
+        $result = $twoSum->twoSum($nums, $target);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function twoSumDataProvider(): array
+    {
+        return [
+            [[2,7,11,15], 9, [0,1]],
+            [[3,2,4], 6, [1,2]],
+            [[3,3], 6, [0,1]]
+        ];
+    }
+}
