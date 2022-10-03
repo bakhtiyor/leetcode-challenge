7df98a6889004cf4f673d9feb528702a726b3e2e
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2022-10-04 01:53:40 +0500

    121. Best Time to Buy and Sell Stock - https://leetcode.com/problems/best-time-to-buy-and-sell-stock/ - Write php unit test - Refactor a code

--- src/BestTimeToBuyAndSellStock.php
+++ src/BestTimeToBuyAndSellStock.php
@@ -1,6 +1,8 @@
 <?php
 
-namespace App\BestTimeToBuyAndSellStock;
+namespace App;
+
+use function App;
 
 /**
  * You are given an array prices where prices[i] is the price of a given stock on the ith day.
@@ -13,8 +15,10 @@ namespace App\BestTimeToBuyAndSellStock;
  * Output: 5
  * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
  * Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  */
-class Solution
+class BestTimeToBuyAndSellStock
 {
     /**
      * @param array<int, int> $prices
