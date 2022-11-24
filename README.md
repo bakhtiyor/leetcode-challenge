049d0be96a913687c381c841aee6084b84833200
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2022-11-25 00:48:29 +0500

    Number of Distinct Averages - https://leetcode.com/problems/number-of-distinct-averages - Code refactoring - Update Readme file

--- README.md
+++ README.md
@@ -3,10 +3,11 @@ I challenge myself to solve at least one problem a day from https://leetcode.com
 
 My account in LeetCode - https://leetcode.com/bakhtiyorbs/ 
 
-Progress: **50 days** / Tasks solved: **54** / Missed days: **3 days**
+Progress: **51 days** / Tasks solved: **55** / Missed days: **3 days**
 
 | Day | Date              | Problem                                                                                                                                    | Level  |
 |-----|-------------------|--------------------------------------------------------------------------------------------------------------------------------------------|--------|
+| 51  | November 24, 2022 | [Number of Distinct Averages](https://leetcode.com/problems/number-of-distinct-averages)                                                   | EASY   |
 | 50  | November 23, 2022 | [3Sum](https://leetcode.com/problems/3sum)                                                                                                 | MEDIUM |
 | 49  | November 22, 2022 | [Climbing Stairs](https://leetcode.com/problems/climbing-stairs)                                                                           | EASY   |
 | 48  | November 21, 2022 | [Pascal's Triangle II](https://leetcode.com/problems/pascals-triangle-ii)                                                                  | EASY   |
--- src/DistinctAveragesNumber.php
+++ src/DistinctAveragesNumber.php
@@ -2,6 +2,29 @@
 
 namespace App;
 
+/**
+ * Number of Distinct Averages
+ *
+ * You are given a 0-indexed integer array nums of even length. As long as nums is not empty, you must repetitively:
+ * Find the minimum number in nums and remove it.
+ * Find the maximum number in nums and remove it.
+ * Calculate the average of the two removed numbers.
+ * The average of two numbers a and b is (a + b) / 2.
+ * For example, the average of 2 and 3 is (2 + 3) / 2 = 2.5.
+ * Return the number of distinct averages calculated using the above process.
+ * Note that when there is a tie for a minimum or maximum number, any can be removed.
+ *
+ * Example 1:
+ * Input: nums = [4,1,4,0,3,5]
+ * Output: 2
+ * Explanation:
+ * 1. Remove 0 and 5, and the average is (0 + 5) / 2 = 2.5. Now, nums = [4,1,4,3].
+ * 2. Remove 1 and 4. The average is (1 + 4) / 2 = 2.5, and nums = [4,3].
+ * 3. Remove 3 and 4, and the average is (3 + 4) / 2 = 3.5.
+ * Since there are 2 distinct numbers among 2.5, 2.5, and 3.5, we return 2.
+ *
+ * https://leetcode.com/problems/number-of-distinct-averages
+ */
 class DistinctAveragesNumber
 {
     /**
@@ -10,6 +33,19 @@ class DistinctAveragesNumber
      */
     public function distinctAverages(array $nums): int
     {
-        return 1;
+        $avgs = [];
+        while (count($nums) > 0) {
+            $max = max($nums);
+            $min = min($nums);
+            $avg = ($max + $min) / 2;
+            if (!in_array($avg, $avgs, true)) {
+                $avgs[] = $avg;
+            }
+            $maxIndex = array_search($max, $nums, true);
+            unset($nums[$maxIndex]);
+            $minIndex = array_search($min, $nums, true);
+            unset($nums[$minIndex]);
+        }
+        return count($avgs);
     }
 }
