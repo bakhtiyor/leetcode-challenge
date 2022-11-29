3f446186adaa8c20f6b9244d9ca4aa0e0c83c7af
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2022-11-29 23:43:31 +0500

    Add Digits - https://leetcode.com/problems/add-digits

---
+++ src/AddDigits.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+/**
+ * Add Digits
+ *
+ * Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+ * Example 1:
+ * Input: num = 38
+ * Output: 2
+ * Explanation: The process is
+ * 38 --> 3 + 8 --> 11
+ * 11 --> 1 + 1 --> 2
+ * Since 2 has only one digit, return it.
+ *
+ * https://leetcode.com/problems/add-digits
+ */
+class AddDigits
+{
+    /**
+     * @param int $num
+     * @return int
+     */
+    public function addDigits(int $num): int
+    {
+        return 1;
+    }
+}
---
+++ tests/AddDigitsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests;
+
+use App\AddDigits;
+use PHPUnit\Framework\TestCase;
+
+class AddDigitsTest extends TestCase
+{
+    /** @dataProvider dataProvider */
+    public function testAddDigits(int $number, int $expected): void
+    {
+        $addDigits = new AddDigits();
+        $result  = $addDigits->addDigits($number);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            [38, 2],
+            [0, 0]
+        ];
+    }
+}
