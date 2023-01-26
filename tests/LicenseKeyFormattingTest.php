a90a18bddd7e44c4fe17da072e14482257b9c13f
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2023-01-26 23:26:05 +0500

    License Key Formatting - https://leetcode.com/problems/license-key-formatting

---
+++ tests/LicenseKeyFormattingTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests;
+
+use App\LicenseKeyFormatting;
+use PHPUnit\Framework\TestCase;
+
+class LicenseKeyFormattingTest extends TestCase
+{
+    /** @dataProvider dataProvider */
+    public function testLicenseKeyFormatting(string $str, int $k, string $expected): void
+    {
+        $licenseKeyFormatting = new LicenseKeyFormatting();
+        $result = $licenseKeyFormatting->licenseKeyFormatting($str, $k);
+
+        $this->assertSame($expected, $result);
+    }
+
+    public function dataProvider(): array
+    {
+        return [
+            ['5F3Z-2e-9-w', 4, '5F3Z-2E9W'],
+            ['2-5g-3-J', 2, '2-5G-3J']
+        ];
+    }
+}
