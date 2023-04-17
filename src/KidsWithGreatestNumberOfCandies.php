d961f80bd7b025fe83fef2102e37681496d9d524
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2023-04-17 23:19:41 +0500

    Kids With the Greatest Number of Candies - https://leetcode.com/problems/kids-with-the-greatest-number-of-candies

---
+++ src/KidsWithGreatestNumberOfCandies.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+/**
+ * Kids With the Greatest Number of Candies
+ *
+ * https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+ */
+class KidsWithGreatestNumberOfCandies
+{
+
+}
