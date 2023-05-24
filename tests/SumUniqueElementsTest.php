5c1f92391cd36167f7dc5d6989347140877d9617
Bakhtiyor Bahritidinov <bakhtiyor.bahriddinov@gmail.com>
2023-05-24 23:51:15 +0500

    Sum of Unique Elements - https://leetcode.com/problems/sum-of-unique-elements

--- tests/SumUniqueElementsTest.php
+++ tests/SumUniqueElementsTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 class SumUniqueElementsTest extends TestCase
 {
     /** @dataProvider dataProvider */
-    public function testSumOfUnique(array $nums, int $expected)
+    public function testSumOfUnique(array $nums, int $expected): void
     {
         $sumUniqueElements = new SumUniqueElements();
         $this->assertEquals($expected, $sumUniqueElements->sumOfUnique($nums));
